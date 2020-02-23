9822782456d941be47439e2405f161411bf5aa5d
Ayrton Casella <awcasella461@gmail.com>
2020-02-22 23:18:36 -0300

    Creation of class user, which uses hibernate to map its attributes to the columns of a DB.

---
+++ src/User.java
@@ -0,0 +1,66 @@
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="login")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    
+    @Column(name="username")
+    private String username;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="password")
+    private String password;
+    
+    @Column(name="fname")
+    private String firstName;
+    
+    @Column(name="lname")
+    private String lastName;
+    
+    public User(){
+        
+    }
+    
+    public User(String username, String email, String password, String firstName, String lastName){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+       
+}
