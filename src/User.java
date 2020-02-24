ddcbc5c972d725ea1a8913b49dc6fce23d0486ac
Ayrton Casella <awcasella461@gmail.com>
2020-02-24 02:55:08 -0300

    Setting username as the PRIMARY KEY.

--- src/User.java
+++ src/User.java
@@ -13,9 +13,6 @@ public class User {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
-    
     @Column(name="username")
     private String username;
     
